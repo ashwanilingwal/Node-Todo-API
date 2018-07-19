55a85a642d42173b71f752cfaf3f928e9563e554
ashwanilingwal <ashwanicodelingwal1998@gmail.com>
2018-07-19 23:05:22 +0530

    testing Get TODOS

--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -1,10 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
-
+//ES6 destructuring
+var {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos=[{
+  _id : new ObjectID(),
   text:'First test todo'
 },
 {
@@ -69,5 +71,33 @@ describe('POST /todos', () => {
     })
     .end(done);
     });
+  });
+  describe('GET /todos/:id',()=>
+  {
+    it('Should return todo doc',(done)=>{
+      request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+        var id1=new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${id1}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return 404 for non object-ids',(done)=>{
+        var id1=new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/abc`)
+        .expect(404)
+        .end(done);
+    });
+
   });
 });
